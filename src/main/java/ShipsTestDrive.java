b5616560dbc789a8a113250807ba520cf6c27228
jabolins <77095873+jabolins@users.noreply.github.com>
2021-01-16 12:04:06 +0200

    Pabeigtas metodes kuģu izvedei

--- src/main/java/ShipsTestDrive.java
+++ src/main/java/ShipsTestDrive.java
@@ -1,6 +1,15 @@
 public class ShipsTestDrive {
     public static void main(String[] args){
 Ships ship = new Ships();
-ship.createShips(10,4,2,2,1);
+boolean izveidesRezultats;
+izveidesRezultats=  ship.createAllShips(4,1,2,3,4);
+        System.out.println("izveides rezultāts ir "+ izveidesRezultats);
+for (int i=0; i<Ships.getAllShips().size(); i++){
+    System.out.print ("Kuģa " + i+ " vietas ir: ");
+    for (int j=0; j<Ships.getAllShips().get(i).getSize(); j++){
+        System.out.print(Ships.getAllShips().get(i).getArea()[j] + "; ");
+    }
+    System.out.println();
+}
     }
 }
